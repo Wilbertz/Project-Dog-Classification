e0a204dbdf1303de92b23f9327b115db24692728
Harald Wilbertz <Harald.Wilbertz@gmx.de>
2019-12-14 17:21:53 +0100

    ResNet and BasicBlock removed to new file.

--- Main.py
+++ Main.py
@@ -4,12 +4,15 @@ import torch
 import numpy as np
 from torch import utils
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.optim as optim
 from torch.optim import lr_scheduler
 from PIL import ImageFile
 from torchvision import transforms, datasets
 import matplotlib.pyplot as plt
+
+import EarlyStopping
+from BasicBlock import BasicBlock
+from ResNet import ResNet
 
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
@@ -74,7 +77,7 @@ def un_normalize(tensors, mean, std):
 
 dataiter = iter(train_loader)
 images, labels = dataiter.next()
-images = un_normalize(images,mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
+images = un_normalize(images, mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
 images = images.numpy()  # convert from tensor to numpy for display
 
 # get class names
@@ -90,106 +93,6 @@ for idx in np.arange(20):
 print(f'Number of dog breeds: {len(train_data.classes)}')
 
 
-def conv3x3(in_planes, out_planes, stride=1):
-    """3x3 convolution with padding"""
-    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
-                     padding=1, bias=False)
-
-
-def conv1x1(in_planes, out_planes, stride=1):
-    """1x1 convolution"""
-    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
-
-
-class BasicBlock(nn.Module):
-
-    def __init__(self, in_planes, planes, stride=1, downsample=None):
-        super(BasicBlock, self).__init__()
-        self.conv1 = conv3x3(in_planes, planes, stride)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = conv3x3(planes, planes)
-        self.bn2 = nn.BatchNorm2d(planes)
-        self.downsample = downsample
-        self.stride = stride
-
-    def forward(self, x):
-        residual = x
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-        out = self.conv2(out)
-        out = self.bn2(out)
-
-        if self.downsample is not None:
-            residual = self.downsample(x)
-
-        out += residual
-        out = self.relu(out)
-
-        return out
-
-class ResNet(nn.Module):
-    ### choose an architecture, and complete the class
-    def __init__(self, block, layers, num_classes=133):
-        super(ResNet, self).__init__()
-        self.inplanes = 16
-
-        self.conv1 = nn.Conv2d(3, 16, kernel_size=7, stride=2, padding=3, bias=False)
-        self.bn1 = nn.BatchNorm2d(16)
-        self.relu = nn.ReLU(inplace=True)
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-
-        self.layer1 = self._make_layer(block, 16, layers[0])
-        self.layer2 = self._make_layer(block, 32, layers[1], stride=2)
-        self.layer3 = self._make_layer(block, 64, layers[2], stride=2)
-        self.layer4 = self._make_layer(block, 128, layers[3], stride=2)
-
-        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = nn.Linear(128, num_classes)
-
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-            elif isinstance(m, nn.BatchNorm2d):
-                nn.init.constant_(m.weight, 1)
-                nn.init.constant_(m.bias, 0)
-
-    def _make_layer(self, block, planes, blocks, stride=1):
-        downsample = None
-        if stride != 1 or self.inplanes != planes:
-            downsample = nn.Sequential(
-                conv1x1(self.inplanes, planes, stride),
-                nn.BatchNorm2d(planes),
-            )
-
-        layers = []
-        layers.append(block(self.inplanes, planes, stride, downsample))
-        self.inplanes = planes
-        for _ in range(1, blocks):
-            layers.append(block(self.inplanes, planes))
-
-        return nn.Sequential(*layers)
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.maxpool(x)
-
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
-
-        x = self.avgpool(x)
-        x = x.view(x.size(0), -1)
-        x = self.fc(x)
-
-        return x
-
-
 model_scratch = ResNet(BasicBlock, [2, 2, 2, 2])
 
 if use_cuda:
@@ -203,43 +106,6 @@ optimizer_scratch = optim.Adam(params=model_scratch.parameters(), lr=0.001)
 
 # reduce learning rate when a validation loss has stopped improving
 plateau_lr_scheduler = lr_scheduler.ReduceLROnPlateau(optimizer_scratch, 'min',  patience=7, verbose=True)
-
-
-class EarlyStopping:
-    """Early stops the training if validation loss dosen't improve after a given patience."""
-
-    def __init__(self, patience=5):
-        """
-        Args:
-            patience (int): How long to wait after last time validation loss improved.
-        """
-        self.patience = patience
-        self.counter = 0
-        self.best_score = None
-        self.early_stop = False
-        self.val_loss_min = np.Inf
-
-    def __call__(self, val_loss, model):
-
-        score = -val_loss
-
-        if self.best_score is None:
-            self.best_score = score
-        elif score < self.best_score:
-            self.counter += 1
-            print(f'EarlyStopping counter: {self.counter} out of {self.patience}')
-            if self.counter >= self.patience:
-                self.early_stop = True
-        else:
-            self.best_score = score
-            self.save_checkpoint(val_loss, model)
-            self.counter = 0
-
-    def save_checkpoint(self, val_loss, model):
-        """Saves model when validation loss decrease."""
-        print(f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
-        torch.save(model.state_dict(), 'saved_models/checkpoint.pt')
-        self.val_loss_min = val_loss
 
 
 def train(n_epochs, loaders, model, optimizer, criterion, use_cuda, save_path, scheduler, patience=15):
